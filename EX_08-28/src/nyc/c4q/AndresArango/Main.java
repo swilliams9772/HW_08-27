052fcb31f0aa2cf7301e224c7c368b36cb4516ac
cito125 <cito125@gmail.com>
2016-08-28 14:26:11 -0400

    Initial commit

---
+++ EX_08-28/src/nyc/c4q/AndresArango/Main.java
@@ -0,0 +1,18 @@
+package nyc.c4q.AndresArango;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public class Student{
+        private String firstName;
+        private String lastName;
+        private String idNumber;
+        private String chicken;
+        private String chocolate;
+        private String plantains;
+
+
+    }
+}
